eb660b3eb70690881619c25d40add95f8a673418
Chinee <Chinehuu@gmail.com>
2022-07-18 12:18:50 +0800

    added function

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ function getRandomCard() {
     }
 }
 
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber > 10) {
+        return 10;
+    }else if (randomNumber === 1) {
+        return 11
+    }else {
+        return randomNumber;
+    }
+}
+
 function startGame() {
     isAlive = true;
     let firstCard = getRandomCard();

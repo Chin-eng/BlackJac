be8c0f67d299a80c0a325a30eb5d98540975aa9b
Chinee <Chinehuu@gmail.com>
2022-07-18 12:24:05 +0800

    deleted function

--- index.js
+++ index.js
@@ -29,17 +29,6 @@ function getRandomCard() {
     }
 }
 
-function getRandomCard() {
-    let randomNumber = Math.floor(Math.random() * 13) + 1;
-    if (randomNumber > 10) {
-        return 10;
-    }else if (randomNumber === 1) {
-        return 11
-    }else {
-        return randomNumber;
-    }
-}
-
 function startGame() {
     isAlive = true;
     let firstCard = getRandomCard();
